983287ff95227ac60d24dfbfe6c93a6d19cf5d8e
Shain Wai Yan <shainwaiyan2002@gmail.com>
2025-05-18 10:56:46 +0630

    change the git server into papawaddy account

--- coding-project.js
+++ coding-project.js
@@ -2,7 +2,7 @@
 const GITHUB_USERNAME = "Shain-Wai-Yan"; // Already set to your username
 
 // Your custom server endpoint
-const SERVER_URL = "https://github-api-server.onrender.com";
+const SERVER_URL = "https://github-api-server-5d05.onrender.com";
 
 // GitHub API endpoints through your proxy server
 const PROFILE_URL = `${SERVER_URL}/api/github/users/${GITHUB_USERNAME}`;
